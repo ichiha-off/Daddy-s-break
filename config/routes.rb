064f47d1120064aac6fea73d376a41711ce3aa54
ichiha-off <bsk.6510riku@gmail.com>
2020-08-23 01:56:55 +0000

    rootTo[/]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'homes#about'
+  get '/' => 'homes#about'
   get 'user/topics/index' => 'users#user_topics'
   get 'search' => 'searches#search'
 
